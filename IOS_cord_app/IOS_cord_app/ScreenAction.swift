be11ca610a1c820878d3a95993373b5f62575a8f
Sapir Shloush <sapir@colu.com>
2019-05-21 13:09:33 +0300

    Update-text-one-new-screen-action

--- IOS_cord_app/IOS_cord_app/ScreenAction.swift
+++ IOS_cord_app/IOS_cord_app/ScreenAction.swift
@@ -12,7 +12,11 @@ class ScreenAction : BaseAction {
 
     override func performAction() {
         let vc = UIViewController()
-        vc.view.backgroundColor = UIColor.red
+        let nextScreenLable = UITextField(frame: CGRect(x: 120, y: 130, width: 173, height: 50))
+        nextScreenLable.textColor = UIColor.black
+        nextScreenLable.text = self.action.body
+        vc.view.addSubview(nextScreenLable)
+        vc.view.backgroundColor = UIColor.white
         self.view.navigationController?.pushViewController(vc, animated: true)
     }
 }
